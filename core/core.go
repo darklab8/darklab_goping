585b90f83d798e063d750f19b17e2d29c0c093fa
dd84ai <dd84ai@gmail.com>
2022-02-05 01:47:43 +0700

    feat: script and package at the same time

---
+++ core/core.go
@@ -0,0 +1,46 @@
+package core
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func query(answer chan string, url string) {
+
+	start := time.Now()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("query failed")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("readAll failed")
+	}
+
+	elapsed := time.Since(start)
+	answer <- string(fmt.Sprintf(string(body), elapsed))
+}
+
+func Queries(channels_amount int, url string) {
+	start := time.Now()
+
+	chans := []chan string{}
+	for i := 0; i < channels_amount; i++ {
+		chans = append(chans, make(chan string))
+	}
+
+	for _, channel := range chans {
+		go query(channel, url)
+	}
+
+	for _, channel := range chans {
+		println(<-channel)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("total time %s\n", elapsed)
+}
--- goping.go
+++ goping.go
@@ -1,52 +1,12 @@
 // Script to query url N times simultaneously
-package goping
+package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 	"strconv"
-	"time"
+
+	"github.com/dd84ai/goping/core"
 )
-
-func query(answer chan string, url string) {
-
-	start := time.Now()
-
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("query failed")
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("readAll failed")
-	}
-
-	elapsed := time.Since(start)
-	answer <- string(fmt.Sprintf(string(body), elapsed))
-}
-
-func Goping(channels_amount int, url string) {
-	start := time.Now()
-
-	chans := []chan string{}
-	for i := 0; i < channels_amount; i++ {
-		chans = append(chans, make(chan string))
-	}
-
-	for _, channel := range chans {
-		go query(channel, url)
-	}
-
-	for _, channel := range chans {
-		println(<-channel)
-	}
-
-	elapsed := time.Since(start)
-	fmt.Printf("total time %s\n", elapsed)
-}
 
 func main() {
 	channels_amount, err := strconv.Atoi(os.Args[1])
@@ -55,5 +15,5 @@ func main() {
 	}
 	url := os.Args[2]
 
-	Goping(channels_amount, url)
+	core.Queries(channels_amount, url)
 }
